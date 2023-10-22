a1cb43a4b193561c5b0cae855d254b5017b266f2
Michele Porcaro <106024283+jamiromic@users.noreply.github.com>
2023-10-22 15:53:09 +0200

    Update Home.js

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -35,7 +35,7 @@ export default class Home extends Component {
     return (
       <div className='container-fluid d-flex justify-content-center flex-column text-center vh-100'>
             <h1>App Students</h1>
-            <span>Created By : <strong>Michele Porcaro | {{currentYear}}</strong></span>
+            <span>Created By : <strong>Michele Porcaro | {currentYear}</strong></span>
         </div>
     );
   }
